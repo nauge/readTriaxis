533b3b55952e8e1656de09691f2acef506383446
Joseph <naugeblue@yahoo.com>
2013-06-30 23:44:22 -0400

    Initial commit
    Read values from three axis accelerometer.

---
+++ src/my/triAxis/readSerial.java
@@ -0,0 +1,148 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.triAxis;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author Joseph Rowland
+ */
+public class readSerial {
+    private static String[] array = null;
+
+    public readSerial()
+    {
+        super();
+    }
+    
+    void connect ( String portName ) throws Exception
+    {
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if ( portIdentifier.isCurrentlyOwned() )
+        {
+            System.out.println("Error: Port is currently in use");
+        }
+        else
+        {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+            
+            if ( commPort instanceof SerialPort )
+            {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+                
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+                               
+                (new Thread(new readSerial.SerialWriter(out))).start();
+                
+                serialPort.addEventListener(new readSerial.SerialReader(in));
+                serialPort.notifyOnDataAvailable(true);
+
+            }
+            else
+            {
+                System.out.println("Error: Only serial ports are handled by this example.");
+            }
+        }     
+    }
+    
+    /**
+     * Handles the input coming from the serial port. A new line character
+     * is treated as the end of a block in this example. 
+     */
+    public static class SerialReader implements SerialPortEventListener 
+    {
+        private InputStream in;
+        private byte[] buffer = new byte[1024];
+        
+        public SerialReader ( InputStream in )
+        {
+            this.in = in;
+        }
+        
+        public void serialEvent(SerialPortEvent arg0) {
+            int data;
+            String robot = null;
+            try
+            {
+                int len = 0;
+                while ( ( data = in.read()) > -1 )
+                {
+                    if ( data == '\n' ) {
+                        break;
+                    }
+                    buffer[len++] = (byte) data;
+                    robot = new String(buffer,0,len);
+                }
+                array = robot.split(" |\\r");
+//                System.out.println(array[0] + " " + array[1] + " " + array[2]);
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+                System.exit(-1);
+            }             
+        }
+
+    }
+
+    /** */
+    public static class SerialWriter implements Runnable 
+    {
+        OutputStream out;
+        
+        public SerialWriter ( OutputStream out )
+        {
+            this.out = out;
+        }
+        
+        public void run ()
+        {
+            try
+            {                
+                int c = 0;
+                while ( ( c = System.in.read()) > -1 )
+                {
+                    this.out.write(c);
+                }                
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+                System.exit(-1);
+            }            
+        }
+    }
+    
+
+    public String[] getArray(){
+        return array;
+    }
+    
+    public void initialize (  )
+    {
+        try
+        {
+            (new readSerial()).connect("COM3");
+        }
+        catch ( Exception e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    
+}
